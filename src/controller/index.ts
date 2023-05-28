0e1a860c27808e390616aa6cc5b46be3d96ace03
Vinicius-de-Morais <vmorais194@gmail.com>
2023-05-28 15:08:08 -0300

    Adicionando rotas e mudando a logica de controller

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -2,7 +2,9 @@ import { UserController } from "./UserController.js";
 import { EventController } from "./EventController.js";
 import { CallController } from "./CallController.js";
 import { SectorController } from "./SectorController.js";
+import { RoleController } from "./RoleController.js";
+import { EmployeeController } from "./EmployeeController.js";
 
 
-const controllers = [UserController, EventController, CallController, SectorController]
+const controllers = [UserController, EventController, CallController, SectorController, RoleController, EmployeeController]
 export default controllers;
