30f9304a42a7c8b792a5563d76e84e2e21a95f67
Vinicius-de-Morais <vmorais194@gmail.com>
2023-09-17 20:16:10 -0300

    Begin the structure of the API return

--- src/service/openaiService.ts
+++ src/service/openaiService.ts
@@ -2,32 +2,77 @@ import OpenAI from "openai";
 import { ChatCompletionCreateParams } from "openai/resources/chat";
 
 export class OpenIaService {
-    private openIAApi: OpenAI;
+	private openIAApi: OpenAI;
 
-    constructor(){
+	constructor() {
+		const config = {
+			apiKey: process.env.OPENAPI_TOKEN,
+		};
 
-        const config = {
-            apiKey: process.env.OPENAPI_TOKEN
-        };
+		this.openIAApi = new OpenAI(config);
+	}
 
-        this.openIAApi = new OpenAI(config);
-    }
-
-
-    /**
+	/**
 	 * Envia para a API da openIA o texto desejado.
 	 * @param text texto a ser enviado.
 	 * @returns retorna a resposta obtida pela API
 	 */
-    public async makeRequest(text: string): Promise<any>{
-        
-        const params:ChatCompletionCreateParams= {
-            model: "gpt-3.5-turbo",
-            messages: [{role: "user", content: text}],
-            functions: [{name: "teste", description: "teste", parameters: {}}]
-        }
+	public async makeRequest(text: string): Promise<any> {
+        const functions = this.defineApiReturn();
 
-        const response = this.openIAApi.chat.completions.create(params);
+		const params: ChatCompletionCreateParams = {
+			model: "gpt-3.5-turbo",
+			messages: [{ role: "user", content: text }],
+			functions: [],
+		};
 
-    }
+		const response = this.openIAApi.chat.completions.create(params);
+	}
+
+	/**
+	 * Retorna a estruturação necessária para o retorno da API.
+	 * @returns retorna a estrutura para a API
+	 */
+	public defineApiReturn(): Object {
+		const params = {
+			type: "object",
+			properties: {
+				theme: {
+					type: "string",
+					description:
+						"This may contain the theme of the lesson plan, like: 'Loop and Conditios in Computer Science'",
+				},
+				objectives: {
+					type: "string",
+					description: "This may contain the objectives aborded in lesson plan",
+				},
+				duration: {
+					type: "string",
+					description: "How many time the class will take",
+				},
+				hability: {
+					type: "string",
+					description: "The hability that the student will improve in this lesson",
+				},
+				method: {
+					type: "string",
+					description: "Witch kind of methodology",
+				},
+				content: {
+					type: "string",
+					description: "The main content of the class",
+				},
+			},
+			required: ["location"],
+		};
+
+		const structure = {
+			name: "returnResponse",
+			description:
+				"This function is responsible to return the function in te corect estructure to frontend",
+			parameters: { params },
+		};
+
+		return structure;
+	}
 }
